75e7d29902f85bc1bf8d44eff0855e397fff08f8
Vutlhari109 <vutlharimathebula74@gmail.com>
2025-03-15 02:25:54 +0200

    Updated app.py with new check_tables route

--- app.py
+++ app.py
@@ -808,5 +808,5 @@ def logout():
 
 if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))  # Default to 5000 if no port is set
-    app.run(host="0.0.0.0", port=port)
+  app.run(host="0.0.0.0", port=port)
 
