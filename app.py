fa5046e7f590715995625ef67d08cb336d037d0e
Vutlhari109 <vutlharimathebula74@gmail.com>
2025-03-15 02:40:24 +0200

    Added password field to User model

--- app.py
+++ app.py
@@ -64,6 +64,7 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False) 
     first_name = db.Column(db.String(120))
     second_name = db.Column(db.String(120))
     surname = db.Column(db.String(120))
